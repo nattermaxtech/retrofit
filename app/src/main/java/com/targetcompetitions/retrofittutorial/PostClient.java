bf871cf08d94144467da8fecb3ff0ebdfb4f6a44
shubham <snshusat161@gmail.com>
2019-05-11 19:22:55 +0530

    Completed Tutorial

--- app/src/main/java/com/targetcompetitions/retrofittutorial/PostClient.java
+++ app/src/main/java/com/targetcompetitions/retrofittutorial/PostClient.java
@@ -15,4 +15,16 @@ public interface PostClient {
     })
     @GET("/posts")
     Call<List<Post>> get();
+
+    /*
+    *
+    * If you want to use dynamic Header then you can give it to the method like
+    *
+    * @GET("/posts")
+    * Call<List<Post>> get(@Header("username") String username);
+     */
+
+    /*
+    * If we add both static and dynamic header then both will be merged.
+     */
 }
